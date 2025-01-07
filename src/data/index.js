45b24fe5482a53675fde2a7de177ceb856b605ab
Cat Miller <catmiller.cm@gmail.com>
2025-01-07 15:42:08 -0600

    add haab months and tzolkin glyphs to data

---
+++ src/data/index.js
@@ -0,0 +1,8 @@
+const haabMonths = ["Pop", "Wo", "Sip", "Sotz'", "Sek", "Xul", "Yaxk'in", "Mol", "Ch'en", "Yax", "Sak'", "Keh", "Mak", "K'ank'in", "Muwan", "Pax", "K'ayab'", "Kumk'u", "Wayeb"];
+
+const tzolkinGlyphs = ["Imix", "Ik'", "Ak'b'al", "K'an", "Chikchan", "Kimi", "Manik'", "Lamat", "Muluk", "Ok", "Chuwen", "Eb'", "Ben", "Ix", "Men", "K'ib'", "Kaban", "Etz'nab'", "Kawak", "Ajaw"];
+
+export {
+  haabMonths,
+  tzolkinGlyphs
+}
