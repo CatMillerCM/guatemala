a5565f42f22d62f4389484a0256aee8e40932c1c
Cat Miller <catmiller.cm@gmail.com>
2025-01-13 18:13:55 -0600

    bring in mayan calendar image and title

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,10 @@
 'use client';
 
+import Image from 'next/image';
 import { useState } from 'react';
 import { DateInput } from '@/components/molecules/date-input';
 import { DateResult } from '@/components/organisms/date-result';
+import mayanCalendar from '@/assets/mayan-calendar.png';
 import styles from './page.module.css';
 
 const Page = () => {
@@ -24,10 +26,19 @@ const Page = () => {
 
   return (
     <main className={styles.main}>
+      <h1 className={styles.title}>Mayan Calendar Converter</h1>
+      <p className={styles.info}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
       <DateInput
         setHaabDate={setHaabDate}
         setTzolkinDate={setTzolkinDate}
       />
+      <div className={styles.calendarContainer}>
+        <Image
+          className={styles.calendarImg}
+          src={mayanCalendar}
+          alt="Mayan Calendar"
+        />
+      </div>
     </main>
   );
 }
