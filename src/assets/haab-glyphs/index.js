f42761ccf40f95a2d1678c16280964a2aad99acb
Cat Miller <catmiller.cm@gmail.com>
2025-01-15 11:36:03 -0600

    add index to import assets

---
+++ src/assets/haab-glyphs/index.js
@@ -0,0 +1,43 @@
+import chen from './chen.png';
+import kankin from './kankin.png';
+import kayab from './kayab.png';
+import keh from './keh.png';
+import kumku from './kumku.png';
+import mak from './mak.png';
+import mol from './mol.png';
+import muwan from './muwan.png';
+import pax from './pax.png';
+import pop from './pop.png';
+import sak from './sak.png';
+import sek from './sek.png';
+import sip from './sip.png';
+import sotz from './sotz.png';
+import wayeb from './wayeb.png';
+import wo from './wo.png';
+import xul from './xul.png';
+import yax from './yax.png';
+import yaxkin from './yaxkin.png';
+
+const haabGlyphImages = {
+  'Ch\'en': chen,
+  'K\'ank\'in': kankin,
+  'K\'ayab': kayab,
+  'Keh': keh,
+  'Kumk\'u': kumku,
+  'Mak': mak,
+  'Mol': mol,
+  'Muwan': muwan,
+  'Pax': pax,
+  'Pop': pop,
+  'Sak': sak,
+  'Sek': sek,
+  'Sip': sip,
+  'Sotz\'': sotz,
+  'Wayeb': wayeb,
+  'Wo': wo,
+  'Xul': xul,
+  'Yax': yax,
+  'Yaxk\'in': yaxkin
+};
+
+export { haabGlyphImages };
---
+++ src/assets/tzolkin-glyphs/index.js
@@ -0,0 +1,45 @@
+import ajaw from './ajaw.png';
+import akbal from './akbal.png';
+import ben from './ben.png';
+import chikchan from './chikchan.png';
+import chuwen from './chuwen.png';
+import eb from './eb.png';
+import etznab from './etznab.png';
+import ik from './ik.png';
+import imix from './imix.png';
+import ix from './ix.png';
+import kaban from './kaban.png';
+import kan from './kan.png';
+import kawak from './kawak.png';
+import kib from './kib.png';
+import kimi from './kimi.png';
+import lamat from './lamat.png';
+import manik from './manik.png';
+import men from './men.png';
+import muluk from './muluk.png';
+import ok from './ok.png';
+
+const tzolkinGlyphImages = {
+  'Ajaw': ajaw,
+  'Ak\'b\'al': akbal,
+  'B\'en': ben,
+  'Chikchan': chikchan,
+  'Chuwen': chuwen,
+  'Eb\'': eb,
+  'Etz\'nab\'': etznab,
+  'Ik\'': ik,
+  'Imix\'': imix,
+  'Ix': ix,
+  'Kab\'an': kaban,
+  'K\'an': kan,
+  'Kawak': kawak,
+  'K\'ib\'': kib,
+  'Kimi': kimi,
+  'Lamat': lamat,
+  'Manik\'': manik,
+  'Men': men,
+  'Muluk': muluk,
+  'Ok': ok
+};
+
+export { tzolkinGlyphImages };
