2b2d28e26905ce112e509de8c0ca0cdef70989b2
Cat Miller <catmiller.cm@gmail.com>
2025-01-15 11:37:51 -0600

    add format gregorian date util

--- src/components/organisms/date-result/index.js
+++ src/components/organisms/date-result/index.js
@@ -1,14 +1,25 @@
 import PropTypes from 'prop-types';
 import { AnotherDateButton } from '@/components/atoms/another-date-button';
 import { MayanDates } from '@/components/atoms/mayan-dates';
-import { TzolkinGlyph } from '@/components/molecules/tzolkin-glyph';
+import { Glyph } from '@/components/molecules/glyph';
+import { formatGregorianDate } from '@/utils/format-gregorian-date';
 import styles from './date-result.module.css';
 
-const DateResult = ({ haabDate, tzolkinDate, setHaabDate, setTzolkinDate }) => {
+const DateResult = ({ haabDate, tzolkinDate, setHaabDate, setTzolkinDate, inputDate }) => {
   return (
-    <div>
-      {tzolkinDate && haabDate && <MayanDates haabDate={haabDate} tzolkinDate={tzolkinDate} />}
-      {tzolkinDate && <TzolkinGlyph tzlolkinGlyph={tzolkinDate.split(' ')[1]} />}
+    <div className={styles.resultPage}>
+      <div className={styles.input}>
+        <p className={styles.intro}>For the date:</p>
+        <p className={styles.gregorianDate}>{formatGregorianDate(inputDate)}</p>
+        <p className={styles.leadIn}>The Mayan Dates are:</p>
+      </div>
+      <div className={styles.output}>
+        {haabDate && tzolkinDate && <MayanDates haabDate={haabDate} tzolkinDate={tzolkinDate} />}
+        <div className={styles.glyphs}>
+          {haabDate && <Glyph glyph={haabDate.split(' ')[1]} />}
+          {tzolkinDate && <Glyph glyph={tzolkinDate.split(' ')[1]} />}
+        </div>
+      </div>
       <AnotherDateButton setHaabDate={setHaabDate} setTzolkinDate={setTzolkinDate}/>
     </div>
   )
@@ -18,7 +29,8 @@ DateResult.propTypes = {
   haabDate: PropTypes.string.isRequired,
   tzolkinDate: PropTypes.string.isRequired,
   setHaabDate: PropTypes.func.isRequired,
-  setTzolkinDate: PropTypes.func.isRequired
+  setTzolkinDate: PropTypes.func.isRequired,
+  inputDate: PropTypes.string.isRequired
 };
 
 export { DateResult };
