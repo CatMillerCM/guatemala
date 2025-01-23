4803d95e7056bfcfed76a4e2081cc557ee58af56
Cat Miller <catmiller.cm@gmail.com>
2025-01-22 23:26:58 -0600

    share button styles in globals

--- src/components/atoms/topic-button/index.js
+++ src/components/atoms/topic-button/index.js
@@ -1,13 +1,11 @@
 import PropTypes from 'prop-types';
 import Link from 'next/link';
-import styles from './topic-button.module.css';
 
 const TopicButton = ({ topic }) => {
   return (
     <Link href={`/${topic.replace(/\s+/g, '-')}`}>
       <button 
         type="button"
-        className={styles.button}
       >{topic}</button>
     </Link>
   )
