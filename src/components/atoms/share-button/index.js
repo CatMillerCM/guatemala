6755c256229db01bd369ece2ae21b657932c41d5
Cat Miller <catmiller.cm@gmail.com>
2025-01-22 19:40:09 -0600

    use html2canvas

--- src/components/atoms/share-button/index.js
+++ src/components/atoms/share-button/index.js
@@ -1,29 +1,24 @@
 'use client';
 
 import PropTypes from 'prop-types';
-import { toPng } from 'html-to-image';
+import html2canvas from 'html2canvas';
 import styles from './share-button.module.css';
 
-// to do - share icon
-// to do - text for share and make prettier
 
 const ShareButton = ({ resultRef }) => {
   const handleShare = async () => {
-      const image = await toPng(resultRef.current);
+    const canvas = await html2canvas(resultRef.current);
 
-      if (navigator.canShare) {
-        const blob = await (await fetch(image)).blob();
-        const file = new File([blob], "mayan-date-result.png", { type: "image/png" });
-
-        await navigator.share({
-          files: [file],
-          title: "My Mayan Glyphs",
-          text: `Check out the Mayan glyphs for my date: `,
-        });
+    canvas.toBlob(async (blob) => {
+      if (blob && navigator.canShare) {
+        const file = new File([blob], 'Mayan Calendar Codex.png', { type: 'image/png' });
+        await navigator.share({ files: [file] });
       } else {
-        alert("Your browser does not support sharing images.");
+        alert('Your browser does not support sharing images.');
       }
+    }, 'image/png');
   };
+  
 
   return (
     <button 
