0b0e0b6fc54d2966875785414060af1c405bfe25
Cat Miller <catmiller.cm@gmail.com>
2025-01-27 01:57:46 -0600

    convert to utc

--- src/utils/get-days-elapsed/index.js
+++ src/utils/get-days-elapsed/index.js
@@ -1,10 +1,10 @@
 const { DateTime } = require('luxon');
 
-const getDaysElapsed = (gregorianDate) => {
-  const gregorianDateLuxon = DateTime.fromISO(gregorianDate, { zone: 'America/Chicago' });
-  const mayanCreationDateLuxon = DateTime.fromObject({ year: -3113, month: 8, day: 11 }, { zone: 'UTC' });
-  
-  const diffInDays = gregorianDateLuxon.diff(mayanCreationDateLuxon, 'days').days;
+const getDaysElapsed = (inputDate) => {
+  const gregorianDate = DateTime.fromISO(inputDate, { zone: 'UTC' }).startOf('day');
+  const mayanCreationDate = DateTime.fromObject({ year: -3113, month: 8, day: 11 }, { zone: 'UTC' }).startOf('day');
+
+  const diffInDays = gregorianDate.diff(mayanCreationDate, 'days').days;
 
   return Math.floor(diffInDays);
 };
